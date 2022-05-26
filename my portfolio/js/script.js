3f999354362662af8e9b5ab74e369d950fec4802
kaisei <102493294+kaiii14@users.noreply.github.com>
2022-05-26 13:44:58 +0900

    commit
    ポートフォリオサイトを公開します。

---
+++ my portfolio/js/script.js
@@ -0,0 +1,43 @@
+'use strict';
+
+function fadeAnime(){
+    $('.fadeRightTrigger').each(function(){ //fadeRightTriggerというクラス名が
+        var elemPos = $(this).offset().top-30;//要素より、50px上の
+        var scroll = $(window).scrollTop();
+        var windowHeight = $(window).height();
+        if (scroll >= elemPos - windowHeight){
+        $(this).addClass('faderight');// 画面内に入ったらfadeRightというクラス名を追記
+        }else{
+        $(this).removeClass('faderight');// 画面外に出たらfadeRightというクラス名を外す
+        }
+        });
+    
+    
+        $('.fadeLeftTrigger').each(function(){ //fadeLeftTriggerというクラス名が
+            var elemPos = $(this).offset().top-30;//要素より、50px上の
+            var scroll = $(window).scrollTop();
+            var windowHeight = $(window).height();
+            if (scroll >= elemPos - windowHeight){
+            $(this).addClass('fadeleft');// 画面内に入ったらfadeLeftというクラス名を追記
+            }else{
+            $(this).removeClass('fadeleft');// 画面外に出たらfadeLeftというクラス名を外す
+            }
+            });
+        
+      
+    
+    }
+    
+      // 画面をスクロールをしたら動かしたい場合の記述
+      $(window).scroll(function (){
+        fadeAnime();/* アニメーション用の関数を呼ぶ*/
+      });// ここまで画面をスクロールをしたら動かしたい場合の記述
+    
+    
+      // 画面が読み込まれたらすぐに動かしたい場合の記述
+      $(window).on('load', function(){
+        fadeAnime();/* アニメーション用の関数を呼ぶ*/
+      });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
+    
+    
+    
