0454ceba3423431eea3a4193029e9d6e998ca39a
kaiii14 <102493294+kaiii14@users.noreply.github.com>
2022-06-09 12:52:18 +0900

    aaaa

--- portfolio/script.js
+++ portfolio/script.js
@@ -1,1 +1,20 @@
 'use strict';
+
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+    },
+    autoplay: {
+        delay: 2500,
+        disableOnInteraction: false,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
