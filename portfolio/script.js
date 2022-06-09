f557a78a9f8734afa5e08be99f8ff8fdcee246b1
kaiii14 <102493294+kaiii14@users.noreply.github.com>
2022-06-09 14:25:41 +0900

    aaa

--- portfolio/script.js
+++ portfolio/script.js
@@ -18,3 +18,22 @@ var swiper = new Swiper(".mySwiper", {
         prevEl: ".swiper-button-prev",
     },
 });
+
+
+let nav =document.getElementById('nav_sp');
+document.getElementById('hamburger_menu').onclick=function(e){
+    nav.classList.toggle("open");
+}
+
+
+nav.querySelector('a').onclick=function(){
+    nav.classList.remove("open");
+}
+
+let nav_a = nav.querySelectorAll('a');
+        for(let i = 0; i < nav_a.length; i++) {
+          nav_a[i].addEventListener('click', function() {
+            // 以下の記述は共通なので関数化をおすすめします。
+            nav.classList.remove("open");
+          });
+        }
