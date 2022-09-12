f8b36e8ce70cd56de0876835450e4b30c77d7d37
kaiii14 <102493294+kaiii14@users.noreply.github.com>
2022-09-12 16:33:27 +0900

    あ

--- script.js
+++ script.js
@@ -49,6 +49,26 @@ $(window).on('load', function () {
 });// ここまで画面が読み込まれたらすぐに動かしたい場合の記述
 
 
+function fadeAnime(){	
+		$('.fade').each(function(){ //fadeというクラス名が
+			var elemPos = $(this).offset().top-100;//要素より、50px上の
+			var scroll = $(window).scrollTop();
+			var windowHeight = $(window).height();
+			if (scroll >= elemPos - windowHeight){
+			$(this).addClass('fadeIn');// 画面内に入ったらfadeLeftというクラス名を追記
+			}else{
+			$(this).removeClass('fadeIn');// 画面外に出たらfadeLeftというクラス名を外す
+			}
+			});
+	}
+
+	// 画面をスクロールをしたら動かしたい場合の記述
+	$(window).scroll(function (){
+		fadeAnime();/* アニメーション用の関数を呼ぶ*/
+	  });// ここまで画面をスクロールをしたら動かしたい場合の記述
+
+  
+
 
 var swiper = new Swiper(".mySwiper", {
     slidesPerView: 1,
